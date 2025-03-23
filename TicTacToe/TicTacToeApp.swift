c2529c0d757921a3274a198814f25387de1768c8
git-Alejandro-star <a13jandro711@icloud.com>
2025-03-23 13:19:22 -0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Alejandro Sanchez on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
